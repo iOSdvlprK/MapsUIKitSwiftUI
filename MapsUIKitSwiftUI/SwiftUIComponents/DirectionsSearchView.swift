8402c2e52be54a25d078bf53ba83883ff85d73ea
iOS developer K <iOSdvlprK@gmail.com>
2023-06-23 23:13:24 +0900

    SwiftUI: transition using NavigationView and NavigationLink with variable bindings

--- MapsUIKitSwiftUI/SwiftUIComponents/DirectionsSearchView.swift
+++ MapsUIKitSwiftUI/SwiftUIComponents/DirectionsSearchView.swift
@@ -20,54 +20,66 @@ struct DirectionsMapView: UIViewRepresentable {
     }
 }
 
-struct DirectionsSearchView: View {
+struct SelectLocationView: View {
+    @Binding var isShowing: Bool
+    
     var body: some View {
-        ZStack(alignment: .top) {
-            VStack(spacing: 0) {
-                VStack {
-                    HStack(spacing: 16) {
-                        Image("start_location_circles")
-                            .frame(width: 24)
-                        HStack {
-                            Text("Source")
-                            Spacer()
+        VStack {
+            Button(action: {
+                // need to dismiss this view
+                self.isShowing = false
+            }, label: {
+                Text("Dismiss")
+            })
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct DirectionsSearchView: View {
+    @State private var isSelectingSource = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .top) {
+                VStack(spacing: 0) {
+                    VStack {
+                        HStack(spacing: 16) {
+                            Image("start_location_circles")
+                                .frame(width: 24)
+                            NavigationLink(destination: SelectLocationView(isShowing: $isSelectingSource), isActive: $isSelectingSource) {
+                                HStack {
+                                    Text("Source")
+                                    Spacer()
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(3)
+                            }
                         }
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(3)
-                    }
-                    HStack(spacing: 16) {
-                        Image("annotation_icon")
-                            .renderingMode(.template)
-                            .foregroundColor(.white)
-                            .frame(width: 24)
-                        HStack {
-                            Text("Destination")
-                            Spacer()
+                        HStack(spacing: 16) {
+                            Image("annotation_icon")
+                                .renderingMode(.template)
+                                .foregroundColor(.white)
+                                .frame(width: 24)
+                            HStack {
+                                Text("Destination")
+                                Spacer()
+                            }
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(3)
                         }
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(3)
                     }
+                    .padding()
+                    .background(Color.blue)
+                    
+                    DirectionsMapView()
+                        .edgesIgnoringSafeArea(.bottom)
                 }
-                .padding()
-                .background(Color.blue)
-                
-                DirectionsMapView()
-                    .edgesIgnoringSafeArea(.bottom)
             }
-            
-            /*
-            // status bar area cover solution (FYI)
-            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-            let window = windowScene?.windows.first
-            VStack {
-                Spacer()
-                    .frame(width: window?.frame.width ?? 0, height: window?.safeAreaInsets.top ?? 0)
-                    .background(Color.red)
-                    .edgesIgnoringSafeArea(.top)
-            }
-            */
+//            .navigationBarTitle("DIRECTIONS")
+//            .navigationBarHidden(true)
         }
     }
 }
