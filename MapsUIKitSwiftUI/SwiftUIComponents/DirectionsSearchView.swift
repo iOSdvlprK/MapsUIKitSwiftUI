b01c2d678da7d625b10e80a1e5f84239d839b649
iOS developer K <iOSdvlprK@gmail.com>
2023-06-24 20:47:07 +0900

    SwiftUI: build search screen

--- MapsUIKitSwiftUI/SwiftUIComponents/DirectionsSearchView.swift
+++ MapsUIKitSwiftUI/SwiftUIComponents/DirectionsSearchView.swift
@@ -21,19 +21,64 @@ struct DirectionsMapView: UIViewRepresentable {
 }
 
 struct SelectLocationView: View {
-//    @Binding var isShowing: Bool
-    @Environment(\.presentationMode) var presentationMode
+    @Binding var isShowing: Bool
+    
+    @State private var mapItems = [MKMapItem]()
+    @State private var searchQuery = ""
     
     var body: some View {
         VStack {
-            Button(action: {
-                // need to dismiss this view
-//                self.isShowing = false
-                self.presentationMode.wrappedValue.dismiss()
-            }, label: {
-                Text("Dismiss")
-            })
+            HStack(spacing: 16) {
+                Button(action: {
+                    self.isShowing = false
+                }, label: {
+                    Image("back_arrow")
+                        .renderingMode(.template)
+                        .foregroundColor(.black)
+                })
+                
+                TextField("Enter search term", text: $searchQuery)
+                    .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification).debounce(for: .milliseconds(500), scheduler: RunLoop.main)) { _ in
+                        // search
+                        let request = MKLocalSearch.Request()
+                        request.naturalLanguageQuery = self.searchQuery
+                        let search = MKLocalSearch(request: request)
+                        search.start { resp, err in
+                            // check the error
+                            self.mapItems = resp?.mapItems ?? []
+                        }
+                    }
+            }
+            .padding()
+            
+            ScrollView {
+                ForEach(mapItems, id: \.self) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("\(item.name ?? "")")
+                                .font(.headline)
+                            Text("\(item.address())")
+                        }
+                        Spacer()
+                    }
+                    .padding()
+                }
+            }
+            
+            Spacer()
         }
+        .edgesIgnoringSafeArea(.bottom)
+        .onAppear(perform: {
+            // dummy search
+//            let request = MKLocalSearch.Request()
+//            request.naturalLanguageQuery = "Food"
+//            let search = MKLocalSearch(request: request)
+//            search.start { resp, err in
+//                // check the error
+//                self.mapItems = resp?.mapItems ?? []
+//            }
+        })
+        .navigationTitle("")
         .navigationBarHidden(true)
     }
 }
@@ -50,25 +95,21 @@ struct DirectionsSearchView: View {
                             Image("start_location_circles")
                                 .frame(width: 24)
                             HStack {
-//                                Text("Source")
                                 Button(action: {
                                     isSelectingSource = true
                                 }, label: {
                                     Text("Source")
+                                    Spacer()
                                 })
                                 .foregroundColor(Color.gray)
-                                Spacer()
                             }
                             .padding()
                             .background(Color.white)
                             .cornerRadius(3)
                         }
-//                        .navigationDestination(isPresented: $isSelectingSource, destination: {
-//                            SelectLocationView(isShowing: $isSelectingSource)
-//                        })
-                        .sheet(isPresented: $isSelectingSource) {
-                            SelectLocationView()
-                        }
+                        .navigationDestination(isPresented: $isSelectingSource, destination: {
+                            SelectLocationView(isShowing: $isSelectingSource)
+                        })
                         HStack(spacing: 16) {
                             Image("annotation_icon")
                                 .renderingMode(.template)
